0da330a9493a7f790e2b8c9159f3aa43288d01ee
xinun <147150326+xinun@users.noreply.github.com>
2025-02-20 13:34:49 +0900

    Update NotificationsScreen.js
    color

--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const notifications = [
+  { id: '1', message: '🔔 your post' },
+  { id: '2', message: '🔔🔔🔔🔔' },
+];
 
 export default function NotificationsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>🔔 Notifications Screen</Text>
+      <FlatList
+        data={notifications}
+        renderItem={({ item }) => (
+          <View style={styles.notificationCard}>
+            <Text style={styles.notificationText}>{item.message}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -12,11 +25,22 @@ export default function NotificationsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#1F2C3D",
+    padding: 10,
   },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
+  notificationCard: {
+    backgroundColor: "#51BCB4",
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  notificationText: {
+    fontSize: 16,
+    color: "#333",
   },
 });
